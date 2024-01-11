035316594b865a5b7f4e6e83f42216a867212a74
ReplacedSpace17 <replacedspace17@gmail.com>
2024-01-11 06:27:03 -0600

    Configuration

--- src/Rutas.jsx
+++ src/Rutas.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom"
 
 import App from "./App.jsx"
 import Login from "./Views/Login/Login.jsx"
+import CrearCuenta from "./Views/Cuenta/CrearCuenta.jsx"
 
 function Rutas() {
   return (
@@ -9,6 +10,7 @@ function Rutas() {
       <Routes>
         <Route path="/" element={ <App /> } />
         <Route path="/Login" element={ <Login /> } />
+        <Route path="/Account" element={ <CrearCuenta /> } />
       </Routes>
     </div>
   )
--- src/Views/Cuenta/CrearCuenta.jsx
+++ src/Views/Cuenta/CrearCuenta.jsx
@@ -1,14 +1,14 @@
 import { useNavigate } from 'react-router-dom';
 import video from '../../assets/Videos/videoLogin.mp4'
 import logo from '../../assets/logo_black.png'
-import './login.css'
+import './crearCuenta.css'
 
 import { useSpring, animated } from '@react-spring/web';
 
-function Login() {
+function CrearCuenta() {
     // Define la animación para la entrada del aside
     const asideAnimation = useSpring({
-      from: { opacity: 0, transform: 'translateX(-100%)' },
+      from: { opacity: 0, transform: 'translateX(+100%)' },
       to: { opacity: 1, transform: 'translateX(0)' },
     });
 
@@ -17,24 +17,23 @@ function Login() {
   const handleClick = () => {
     navigate('/Login');
   }
-  const handleCreateCuenta = () => {
-    navigate('/Account');
-  }
-  
+
   return (
-    <body className='bodyApp'>
+    <body className='bodyAppAcount'>
       <animated.aside style={asideAnimation} className="aside-container">
         <img src={logo} alt="Logo" className="logoLogin" />
         <p className='parrafo'>Para continuar, por favor identifícate...</p>
         <div className="containerTitulo">
-          <div className="line"></div>
-          <h2 className='TITLE-Login'>Inicia Sesión</h2>
-          <div className="line"></div>
+          <h2 className='TITLE-Login'>Crea tu cuenta</h2>
         </div>
+        <input type="text" className='inputLogin' placeholder="example@gmail.com" />
+        <input type="text" className='inputLogin' placeholder="example@gmail.com" />
+        <input type="text" className='inputLogin' placeholder="example@gmail.com" />
+        <input type="text" className='inputLogin' placeholder="example@gmail.com" />
         <input type="text" className='inputLogin' placeholder="example@gmail.com" />
         <input type='password' className='inputLogin' placeholder="*********" />
         <p className='parrafo'>¿Olvidaste tu contraseña?</p>
-        <div className="containerCrearCuenta"><p className='parrafo'>¿No tienes cuenta?  </p><p className='RegistrateAqui' onClick={handleCreateCuenta}>Registrate aquí</p></div>
+        <div className="containerCrearCuenta"><p className='parrafo'>¿No tienes cuenta?  </p><p className='RegistrateAqui'>Registrate aquí</p></div>
         <button className='buttonLogin' onClick={handleClick}>Iniciar Sesión</button>
       </animated.aside>
       <div id="video-background">
@@ -50,4 +49,4 @@ function Login() {
   )
 }
 
-export default Login
+export default CrearCuenta
